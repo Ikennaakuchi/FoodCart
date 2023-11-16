b1694639ff854138417a4367d0e4c40030f37b0e
Akuchi Ikenna <akuchiikenna16@gmail.com>
2023-11-16 10:01:49 +0100

    Implementing Order Domain Service

---
+++ order-service/order-domain/order-domain-core/src/main/java/org/food/ordering/system/order/service/domain/entity/Restaurant.java
@@ -0,0 +1,60 @@
+package org.food.ordering.system.order.service.domain.entity;
+
+import org.food.ordering.system.domain.entity.AggregateRoot;
+import org.food.ordering.system.domain.valueobject.RestaurantId;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Restaurant extends AggregateRoot<RestaurantId> {
+
+    private final List<Product> productList;
+    private boolean active;
+
+    private Restaurant(Builder builder) {
+        super.setId(builder.restaurantId);
+        productList = builder.productList;
+        active = builder.active;
+    }
+
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public static final class Builder {
+        private RestaurantId restaurantId;
+        private List<Product> productList;
+        private boolean active;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder restaurantId(RestaurantId val) {
+            restaurantId = val;
+            return this;
+        }
+
+        public Builder productList(List<Product> val) {
+            productList = val;
+            return this;
+        }
+
+        public Builder active(boolean val) {
+            active = val;
+            return this;
+        }
+
+        public Restaurant build() {
+            return new Restaurant(this);
+        }
+    }
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/org/food/ordering/system/order/service/domain/event/OrderCanceledEvent.java
@@ -0,0 +1,11 @@
+package org.food.ordering.system.order.service.domain.event;
+
+import org.food.ordering.system.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCanceledEvent extends OrderEvent{
+    public OrderCanceledEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
